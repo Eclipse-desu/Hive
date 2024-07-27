5b439afe5e90eb6ccfe00d6735cd35f790b97d1a
Eclipse_desu <hilberto.eclipse@gmail.com>
2024-07-27 15:42:35 +0800

    save for pulling.

---
+++ code.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int a, b;
+    // a x b
+    // scanf("%d%d", &a, &b);
+    // read in?
+    double pi = 3.1415926535897932384626433832795028841971;
+    printf("%.4lf", pi);
+    return 0;
+}
+
+std::cin
+
+// f = format 格式
+// int = integer 整数
+// \n : \ 转义
+// \n : newline
+// %d : d = digit / dec
+// %s : string
+// %c : character
+//  x : Hexdecimal 16进制
+// %lf: long float = double
+//  & : 取地址
